59d0032311c73f37ef7245f7694504539d3926e7
k50044091 <kangli13@huawei.com>
2024-08-30 17:11:50 +0800

    fix copyright

--- example/ops/unpad/op_kernel/unpad.cpp
+++ example/ops/unpad/op_kernel/unpad.cpp
@@ -1,7 +1,6 @@
-
 /*
  * Copyright (c) 2024 Huawei Technologies Co., Ltd.
- * AscendOpCommonLib is licensed under Mulan PSL v2.
+ * MindKernelInfra is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *          http://license.coscl.org.cn/MulanPSL2
