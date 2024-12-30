1cfbb181caeb64724c7a95d3e826ec9aa97c33da
y30064824 <yangbochu1@h-partners.com>
2024-12-30 15:05:10 +0800

    clean code

--- src/utils/rt/device/device.cpp
+++ src/utils/rt/device/device.cpp
@@ -7,7 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
- #include <acl/acl.h>
+#include <acl/acl.h>
 #include "mki/utils/rt/device/device.h"
 #include "mki/utils/rt/backend/backend_factory.h"
 #include "mki/utils/log/log.h"
