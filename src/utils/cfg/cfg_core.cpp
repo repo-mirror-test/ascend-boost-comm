810833e0b41295e984155a74043234b79a2f46de
Lord_of_Ironhill <suiweiyi@huawei.com>
2025-01-22 17:17:21 +0800

    compile fail

--- src/utils/cfg/cfg_core.cpp
+++ src/utils/cfg/cfg_core.cpp
@@ -56,7 +56,7 @@ Status CfgCore::ParseLogData()
     std::map<std::string, std::map<std::string, std::string>>::iterator it = rawInfoMap_.find("LOG");
     if (it == rawInfoMap_.end()) {
         if (printLog_) {
-            std::cout << "ParseLogData failed!" << std::enndl;
+            std::cout << "ParseLogData failed!" << std::endl;
         }
         return Status::FailStatus(ERROR_INVALID_VALUE);
     }
