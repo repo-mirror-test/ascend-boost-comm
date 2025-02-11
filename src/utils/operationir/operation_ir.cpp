21f814bc2f2cab40a4862a8f242c680e214948f6
qiuqianjin <qiuqianjin@huawei.com>
2025-02-11 09:12:27 +0000

    [task] fix operation ir
    Signed-off-by: qiuqianjin <qiuqianjin@huawei.com>

--- src/utils/operationir/operation_ir.cpp
+++ src/utils/operationir/operation_ir.cpp
@@ -1,17 +1,18 @@
 /*
  * Copyright (c) 2024 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+ * MindKernelInfra is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
  */
 #include "mki/utils/operationir/operation_ir.h"
 #include <string>
 #include "mki/utils/log/log.h"
 #include "mki/utils/assert/assert.h"
-#include <iostream>
 
 namespace Mki {
 static const int MAX_TENSOR_INDEX = 48;
@@ -269,7 +270,6 @@ bool OperationIr::ExtendInTensorIrByInputlens(SVector<int> &inputlens)
         }
         totalLen += len;
     }
-    std::cout << "totalLen" << totalLen << std::endl;
     int startId = totalLen;
     inTensorInfoIrs_.resize(totalLen);
     for (int i = oldLen - 1; i >= 0 ; i--) {
@@ -291,7 +291,6 @@ bool OperationIr::ExtendOutTensorIrByOutputlens(SVector<int> &outputlens)
         }
         totalLen += len;
     }
-    std::cout << "totalLen" << totalLen << std::endl;
     int startId = totalLen;
     outTensorInfoIrs_.resize(totalLen);
     for (int i = oldLen - 1; i >= 0 ; i--) {
